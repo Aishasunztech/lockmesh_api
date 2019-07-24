107e110d6df3a90a34fa677c01fe5cddbb78d09f
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2019-07-24 16:15:40 +0500

    fix stackify APM, added stackify logger.

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
-require('express-group-routes');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+require("express-group-routes");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-var swaggerUi = require('swagger-ui-express'),
-swaggerDocument = require('./swagger.json');
+var swaggerUi = require("swagger-ui-express"),
+	swaggerDocument = require("./swagger.json");
+
+var stackify = require("stackify-logger");
 
 var app = express();
-app.disable('etag');
-app.options('*', cors());
+app.disable("etag");
+app.options("*", cors());
+
+var serverEnv = "localhost";
+if (process.env.HOST_NAME) serverEnv = process.env.HOST_NAME;
+stackify.start({
+	apiKey: "8Yl8Cw1Cv4Df8Tl3Jk3Ne1Yf1Qz7Sm2Mn0Aj6An",
+	appName: "LockMesh",
+	env: serverEnv
+});
+app.use(stackify.expressExceptionHandler);
 
 // url logging
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // app.use(bodyParser.json({limit: "1000gb"}));
 // app.use(bodyParser.urlencoded({limit: "1000gb", extended: false, parameterLimit:100000000}));
 
 app.use(express.json({ limit: "1000gb" }));
-app.use(express.urlencoded({ limit: "1000gb", extended: false, parameterLimit: 100000000 }));
+app.use(
+	express.urlencoded({
+		limit: "1000gb",
+		extended: false,
+		parameterLimit: 100000000
+	})
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors("*"));
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Accept-Encoding', 'gzip,sdch');
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-		res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Accept-Encoding", "gzip,sdch");
+	if (req.method === "OPTIONS") {
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+		);
+		res.header(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, DELETE, PATCH"
+		);
 		return res.status(200).json({});
-	};
+	}
 
 	next();
 });
 
 // routes
-app.get('/', function(req, res){
+app.get("/", function(req, res) {
 	res.send("Express");
-})
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-require('./routes/index.js')(app);
-
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+require("./routes/index.js")(app);
 
 module.exports = app;
--- stackify.js
+++ stackify.js
@@ -1,18 +1,11 @@
-
 /**
  * Stackify Node APM Configuration
  */
 
-if(process.env.HOST_NAME) const serverEnv = process.env.HOST_NAME;
-else const serverEnv = 'localhost';
+var serverEnv = "localhost";
+if (process.env.HOST_NAME) serverEnv = process.env.HOST_NAME;
 
 exports.config = {
-	/**
-	 * Your application name.
-	 */
 	application_name: "LockMesh",
-	/**
-	 * Your environment name.
-	 */
 	environment_name: serverEnv
 };
