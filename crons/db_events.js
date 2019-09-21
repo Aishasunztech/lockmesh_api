62c6dbd4b98e4a4035ad3cb56e89136d61f074f1
Usman <usman@sunztech.com>
2019-09-21 15:48:20 +0500

    import socket in db event

--- crons/db_events.js
+++ crons/db_events.js
@@ -1,26 +1,31 @@
 var { DBEvents } = require('../config/database');
 const MySQLEvents = require('@rodrigogs/mysql-events');
 const constants = require('../config/constants');
+const sockets = require('../routes/sockets');
 
 exports.deviceQueue = async function () {
     await DBEvents.start();
-    
+
     DBEvents.addTrigger({
         name: 'device_queue',
         expression: `${constants.DB_NAME}.device_history`,
         statement: MySQLEvents.STATEMENTS.ALL,
         onEvent: (event) => { // You will receive the events here
-            console.log('queue: ',event.affectedRows);
+            // console.log('queue: ', event.affectedRows);
+            console.log('socket:', sockets);
+
         },
     });
 
     DBEvents.addTrigger({
-        name:'check_device_online',
+        name: 'check_device_online',
         expression: `${constants.DB_NAME}.devices`,
         statement: MySQLEvents.STATEMENTS.UPDATE,
         // type: 'UPDATE',
         onEvent: (event) => {
-            console.log('device online:', event);
+            if(event.type === 'UPDATE' && event.affectedColumns.includes('online')){
+                console.log(sockets);
+            }
         }
     });
     DBEvents.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
