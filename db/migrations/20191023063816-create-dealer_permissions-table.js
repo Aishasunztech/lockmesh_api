3e49e1a3e1a62ef2cf5f248e1ac84c3533ef949e
Usman <usman@sunztech.com>
2019-10-23 11:49:03 +0500

    dealer permission migrations

---
+++ db/migrations/20191023063816-create-dealer_permissions-table.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('dealer_permissions', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      permission_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      dealer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      permission_type: {
+        type: Sequelize.ENUM,
+        values: ['apk','policy', 'domain']
+      },
+      created_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: 'TIMESTAMP',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+    }).then(function(){
+      return queryInterface.addConstraint('dealer_permissions', ['permission_id', 'dealer_id', 'permission_type'], {
+        type: 'unique',
+        name: 'unique_permission_id_dealer_id_permission_type'
+      })
+    })
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
