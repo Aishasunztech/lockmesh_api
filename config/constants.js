a8351fc69355e920195b6a285e67e8a846b07e96
Usman <usman@sunztech.com>
2019-11-06 12:51:44 +0500

    constant file indentation

--- config/constants.js
+++ config/constants.js
@@ -86,68 +86,68 @@ if (HOST_NAME) {
 // 	// APP_TITLE = 'LockMesh'
 // }
 
-(module.exports.SUPERADMIN_URL = SUPERADMIN_URL),
-	(module.exports.SUPERADMIN_URL_API = SUPERADMIN_URL_API),
-	(module.exports = {
-		APP_TITLE: APP_TITLE,
-		HOST: URL,
-		PORT: "",
-		SECRET: "kepitsecretwithauth!@#",
-		EXPIRES_IN: "86400s", // 24 Hours
-		DASHBOARD_EXPIRES_IN: '10800s', //3 Hours
-		MOBILE_EXPIRES_IN: '10800s', //3 Hours
-		BASIC_AUTH_USER: "web",
-		BASIC_AUTH_PASSWORD: "Google!@#1",
+module.exports.SUPERADMIN_URL = SUPERADMIN_URL
+module.exports.SUPERADMIN_URL_API = SUPERADMIN_URL_API
+module.exports = {
+	APP_TITLE: APP_TITLE,
+	HOST: URL,
+	PORT: "",
+	SECRET: "kepitsecretwithauth!@#",
+	EXPIRES_IN: "86400s", // 24 Hours
+	DASHBOARD_EXPIRES_IN: '10800s', //3 Hours
+	MOBILE_EXPIRES_IN: '10800s', //3 Hours
+	BASIC_AUTH_USER: "web",
+	BASIC_AUTH_PASSWORD: "Google!@#1",
 
-		// SMTP Constants
-		SMTP_HOST: "smtp.office365.com",
-		SMTP_PORT: 587,
-		SMTP_USERNAME: "admin@lockmesh.com",
-		SMTP_PASSWORD: "34e@2!2xder",
+	// SMTP Constants
+	SMTP_HOST: "smtp.office365.com",
+	SMTP_PORT: 587,
+	SMTP_USERNAME: "admin@lockmesh.com",
+	SMTP_PASSWORD: "34e@2!2xder",
 
-		SMTP_FROM_EMAIL: SMTP_FROM_EMAIL,
-		SMTP_FROM_NAME: SMTP_FROM_NAME,
-		SMTP_COMMON_SUBJECT: `${APP_TITLE} -`,
+	SMTP_FROM_EMAIL: SMTP_FROM_EMAIL,
+	SMTP_FROM_NAME: SMTP_FROM_NAME,
+	SMTP_COMMON_SUBJECT: `${APP_TITLE} -`,
 
-		// Database Constants
-		DB_HOST: DB_HOST,
-		DB_NAME: DB_NAME,
-		DB_USERNAME: DB_USERNAME,
-		DB_PASSWORD: DB_PASSWORD,
+	// Database Constants
+	DB_HOST: DB_HOST,
+	DB_NAME: DB_NAME,
+	DB_USERNAME: DB_USERNAME,
+	DB_PASSWORD: DB_PASSWORD,
 
-		DEVICE_ID_SYSTEM_LETTER,
-		DEVICE_ID_SYSTEM_LETTER_INDEX,
+	DEVICE_ID_SYSTEM_LETTER,
+	DEVICE_ID_SYSTEM_LETTER_INDEX,
 
-		DEALER_PIN_SYSTEM_LETTER,
-		DEALER_PIN_SYSTEM_LETTER_INDEX,
+	DEALER_PIN_SYSTEM_LETTER,
+	DEALER_PIN_SYSTEM_LETTER_INDEX,
 
-		STAFF_ID_SYSTEM_LETTER,
-		STAFF_ID_SYSTEM_LETTER_INDEX,
+	STAFF_ID_SYSTEM_LETTER,
+	STAFF_ID_SYSTEM_LETTER_INDEX,
 
-		// Fixer API key
-		FIXER_API_KEY: "96035c5c5b46baea5a96b84930eaed79",
-		BASE_CURRENCY: "USD",
+	// Fixer API key
+	FIXER_API_KEY: "96035c5c5b46baea5a96b84930eaed79",
+	BASE_CURRENCY: "USD",
 
-		// SUPERADMIN URLs
-		SUPERADMIN_LOGIN_URL: `${
-			this.SUPERADMIN_URL_API
-			}/api/v1/auth/white_label_login`,
-		UPDATE_DEVICE_SUPERADMIN_URL: `${
-			this.SUPERADMIN_URL_API
-			}/api/v1/users/update_device_details`,
-		ADD_CREDITS_SALE_RECORD: `${
-			this.SUPERADMIN_URL_API
-			}/api/v1/users/add_credits_sale_record`,
-		REQUEST_FOR_CREDITS: `${
-			this.SUPERADMIN_URL_API
-			}/api/v1/users/request_for_credits`,
+	// SUPERADMIN URLs
+	SUPERADMIN_LOGIN_URL: `${
+		this.SUPERADMIN_URL_API
+		}/api/v1/auth/white_label_login`,
+	UPDATE_DEVICE_SUPERADMIN_URL: `${
+		this.SUPERADMIN_URL_API
+		}/api/v1/users/update_device_details`,
+	ADD_CREDITS_SALE_RECORD: `${
+		this.SUPERADMIN_URL_API
+		}/api/v1/users/add_credits_sale_record`,
+	REQUEST_FOR_CREDITS: `${
+		this.SUPERADMIN_URL_API
+		}/api/v1/users/request_for_credits`,
 
-		RESYNC_IDS: `${
-			this.SUPERADMIN_URL_API
-			}/api/v1/users/resync_ids`,
+	RESYNC_IDS: `${
+		this.SUPERADMIN_URL_API
+		}/api/v1/users/resync_ids`,
 
-		SUPERADMIN_USER_CREDENTIALS: {
-			email: "whiteLabelAdmin!786@gmial.com",
-			password: "whiteLabel@Admin!786"
-		}
-	});
+	SUPERADMIN_USER_CREDENTIALS: {
+		email: "whiteLabelAdmin!786@gmial.com",
+		password: "whiteLabel@Admin!786"
+	}
+};
