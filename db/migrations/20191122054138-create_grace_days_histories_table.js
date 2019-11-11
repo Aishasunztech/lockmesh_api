dc4d25289d7cb6dd435840423ff5c11eb0d04914
Bilal <bilal.bs143@gmail.com>
2019-11-11 18:05:45 +0500

    migration for grace days

---
+++ db/migrations/20191122054138-create_grace_days_histories_table.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('grace_days_histories').then(function () {
+      queryInterface.createTable('grace_days_histories', {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        dealer_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        },
+        device_id: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        user_acc_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        grace_days: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        from_date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        to_date: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        created_at: {
+          type: 'TIMESTAMP',
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updated_at: {
+          type: 'TIMESTAMP',
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+        },
+      })
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
