d35904e38d29ffd3309134a9aa7de044002c187c
HamzaDawood <hamza.dawood007@gmail.com>
2019-12-12 16:14:55 +0500

    generate PGP email automatically

--- app/controllers/services.js
+++ app/controllers/services.js
@@ -18,21 +18,66 @@ exports.createServiceProduct = async function (req, res) {
         let product_data = req.body.product_data
         let type = req.body.type
         if (type && auto_generated && product_data) {
+            if (type === 'pgp_email' && !product_data.auto_generated) {
+                let pgp_email = product_data.username + '@' + product_data.domain
+                console.log(pgp_email);
+                if (helpers.validateEmail(pgp_email)) {
+                    let checkExisted = await sql.query(`SELECT * FROM pgp_emails WHERE pgp_email = '${pgp_email}'`)
+                    if (checkExisted && checkExisted.length) {
+                        res.send({
+                            status: false,
+                            msg: "ERROR: Username not available.Please choose another username."
+                        })
+                        return
+                    }
+                    product_data.pgp_email = pgp_email
+                } else {
+                    res.send({
+                        status: false,
+                        msg: "ERROR: Invalid pgp email."
+                    })
+                    return
+                }
+            }
             axios.post(app_constants.SUPERADMIN_LOGIN_URL, app_constants.SUPERADMIN_USER_CREDENTIALS, { headers: {} }).then((response) => {
                 if (response.data.status) {
                     let data = {
                         auto_generated,
                         product_data,
-                        type
+                        type,
+                        label: app_constants.APP_TITLE
                     }
                     axios.post(app_constants.CREATE_SERVICE_PRODUCT, data, { headers: { authorization: response.data.user.token } }).then(async function (response) {
                         if (response.data.status) {
-
+                            if (type === 'pgp_email') {
+                                sql.query(`INSERT INTO pgp_emails (pgp_email) VALUES ('${product_data.pgp_email}')`, function (err, results) {
+                                    if (err) {
+                                        console.log(err);
+                                        res.send({
+                                            status: false,
+                                            msg: "ERROR: Internal Server Error. Please Try again."
+                                        })
+                                        return
+                                    }
+                                    if (results && results.insertId) {
+                                        res.send({
+                                            status: true,
+                                            msg: "Pgp Email has been created Successfully."
+                                        })
+                                        return
+                                    } else {
+                                        res.send({
+                                            status: false,
+                                            msg: "Pgp Email not created. Please Try again."
+                                        })
+                                        return
+                                    }
+                                })
+                            }
                         } else {
-                            console.log(err);
                             res.send({
                                 status: false,
-                                msg: "ERROR: Superadmin server not responding please try again later."
+                                msg: response.data.msg
                             })
                             return
                         }
