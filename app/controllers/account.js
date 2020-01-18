809cc52ff191d2006eb17f31444006f1c7ffde7c
HamzaDawood <hamza.dawood007@gmail.com>
2020-01-18 16:47:38 +0500

    pgp email already taken fix

--- app/controllers/account.js
+++ app/controllers/account.js
@@ -547,10 +547,10 @@ exports.getPGPEmails = async (req, res) => {
 
         let query = '';
         if (permission_ids.length) {
-            query = `SELECT * FROM pgp_emails WHERE used=0 AND domain_id IN (${permission_ids.join()})`;
+            query = `SELECT * FROM pgp_emails WHERE used=0 AND delete_status = 0 AND domain_id IN (${permission_ids.join()})`;
         }
         // query = `SELECT * FROM pgp_emails WHERE used=0`;
-
+        console.log(query);
         if (query !== '') {
             let resp = await sql.query(query);
             res.send({
