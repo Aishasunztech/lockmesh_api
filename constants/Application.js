68e7ce550715bc05db6c7317be53e9a38629b6d3
Abaidullah <abaid@sunztech.com>
2020-01-28 16:54:49 +0500

    replace the name from s-dealer to "parent dealer id"  and from s-delaer name to "parent dealer name"

--- constants/Application.js
+++ constants/Application.js
@@ -202,8 +202,10 @@ module.exports = {
     { "key": "sim_2", "value": "tableHeadings.SIM2" },
     { "key": "serial_number", "value": "tableHeadings.SERIALNUMBER" },
     { "key": "model", "value": "tableHeadings.MODEL" },
-    { "key": "s_dealer", "value": "tableHeadings.S-DEALER" },
-    { "key": "s_dealer_name", "value": "tableHeadings.S-DEALERNAME" },
+    // { "key": "s_dealer", "value": "tableHeadings.S-DEALER" },
+    // { "key": "s_dealer_name", "value": "tableHeadings.S-DEALERNAME" },
+    { "key": "s_dealer", "value": "tableHeadings.device-parent-id" }, // PARENT DEALER ID
+    { "key": "s_dealer_name", "value": "tableHeadings.device-parent-name" }, // PARENT DEALER NAME
     { "key": "remainTermDays", "value": "REMAINING TERM DAYS" },
     { "key": "start_date", "value": "tableHeadings.STARTDATE" },
     { "key": "expiry_date", "value": "tableHeadings.EXPIRYDATE" },
