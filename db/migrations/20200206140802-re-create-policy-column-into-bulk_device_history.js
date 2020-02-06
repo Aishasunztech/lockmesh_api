fb3dc3226221cc00bfee8badfd8bce8feb89f50a
Abaidullah <abaid@sunztech.com>
2020-02-06 19:12:41 +0500

    migration to fix policy column type

---
+++ db/migrations/20200206140802-re-create-policy-column-into-bulk_device_history.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.renameColumn('bulk_device_history', 'policy', 'old_policy', {
+      type: Sequelize.INTEGER
+    }).then(function () {
+      return queryInterface.addColumn('bulk_device_history', 'policy', {
+        type: Sequelize.INTEGER,
+        allowNull: true
+      })
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
