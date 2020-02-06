12d1f8271e5acf66b0acd409bbe0ef42c7a95f1a
Abaidullah <abaid@sunztech.com>
2020-02-06 19:18:30 +0500

    FIX

--- db/migrations/20200206140802-re-create-policy-column-into-bulk_device_history.js
+++ db/migrations/20200206140802-re-create-policy-column-into-bulk_device_history.js
@@ -3,7 +3,8 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.renameColumn('bulk_device_history', 'policy', 'old_policy', {
-      type: Sequelize.INTEGER
+      type: Sequelize.TEXT,
+      defaultValue: null
     }).then(function () {
       return queryInterface.addColumn('bulk_device_history', 'policy', {
         type: Sequelize.INTEGER,
